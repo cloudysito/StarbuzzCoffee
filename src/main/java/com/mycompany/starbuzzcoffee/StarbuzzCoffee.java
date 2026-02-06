750901019cf10fbb42e22aa72a1cf88e471e01b1
emiliano <emiimarqz@gmail.com>
2026-02-05 20:40:34 -0700

    Initial commit

---
+++ src/main/java/com/mycompany/starbuzzcoffee/StarbuzzCoffee.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.starbuzzcoffee;
+
+/**
+ *
+ * @author emiim
+ */
+public class StarbuzzCoffee {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
